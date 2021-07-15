8dfd13a3ea57ed9aea2291439313d1b90ab20f3a
Mykola Kyrylenko <bulletbest@gmail.com>
2021-07-16 00:23:16 +0300

    last fix?

--- src/Statistics/Statistics.js
+++ src/Statistics/Statistics.js
@@ -3,7 +3,9 @@ import s from './Statistics.module.css';
 function Statistics({ title, stats, color }) {
     return (
         <section className={s.statistics}>
-            <h2 className={s.title}>{title}</h2>
+
+            {title && <h2 className={s.title}>{title}</h2>}
+            
 
             <ul className={s.statList}>
                 {stats.map(stat => (
